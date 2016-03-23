56269c51dfe2eeb928a7a10451f0879cf1ac93cf
Andrea di Cagno <andreadicagno@gmail.com>
2016-03-23 22:59:27 +0100

    getAll bug fix

--- source/index.js
+++ source/index.js
@@ -5,11 +5,10 @@ import {
 import util from 'util'
 import request from 'request'
 import querystring from 'querystring'
-import async from 'async'
 
 /**
  * Get All Subscribed Pulses from OTX
- * @param  {object} params Object that contains: apikey, [modified_since]
+ * @param  {object} params Object that contains: apikey, [modified_since], [limit]
  * @return {stream} rs     Readable stream that returns an array of objects
  */
 let getAll = (params) => {
@@ -37,42 +36,34 @@ let getAll = (params) => {
   }
 
   rs._read = () => {
-    async.whilst(
-      () => {
-        return options.url
-      },
-      (callback) => {
-        request(options, (error, response, body) => {
-          if (!error && response.statusCode === 200) {
-            let info = JSON.parse(body)
-            rs.count = info.count
-            options.url = info.next
-            rs.push(info.results)
-            callback()
-          } else {
-            return rs.emit('error', new Error(`Connection Error (${response.statusCode}), check the API KEY`))
-          }
-        })
-      },
-      (err) => {
-        if (err) {
-          return rs.emit('error', err)
+    request(options, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        let info = JSON.parse(body)
+        options.url = info.next
+        rs.push(info.results)
+        if (!options.url) {
+          rs.push(null)
         }
-        rs.push(null)
+      } else {
+        if (error) {
+          return rs.emit('error', error)
+        } else {
+          return rs.emit('error', new Error(`Connection Error (${response.statusCode}), check the API KEY`))
+        }
       }
-    )
+    })
   }
-
   return rs
 }
 
 
 /**
  * Extract IOC from getAll stream
- * @param  {array}  ...type  Array that defines the IOC types
- * @return {stream} tr       Transform stream that returns an array of objects 
+ * @param  {array}    types   Array that defines the IOC types
+ * @param  {boolean}  iocOnly Defines if it returns only the IOC indicators
+ * @return {stream}   tr      Transform stream that returns an array of objects
  */
-var getIOC = (...type) => {
+var getIOC = (types = ['IPv4', 'domain'], iocOnly = false) => {
 
   var tr = new Transform({
     objectMode: true
@@ -80,13 +71,18 @@ var getIOC = (...type) => {
 
   tr._transform = function(chunk, encoding, done) {
     chunk.forEach((elem) => {
-      elem.indicators.filter((elem) => type.indexOf(elem.type) !== -1).forEach(
-        (elem) => tr.push(elem)
+      elem.indicators.filter((elem) => types.indexOf(elem.type) !== -1).forEach(
+        (elem) => {
+          if (iocOnly) {
+            tr.push(elem.indicator)
+          } else {
+            tr.push(elem)
+          }
+        }
       )
     })
     done()
   }
-
   return tr;
 }
 
