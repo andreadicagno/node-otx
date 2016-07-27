84ef849b28e7cd65a9d384b52ac21560da18a44a
Andrea di Cagno <andreadicagno@gmail.com>
2016-07-27 13:21:05 +0200

    new file:   test/mocha.opts modified:   test/test.js

--- test/test.js
+++ test/test.js
@@ -1,15 +1,44 @@
-var assert = require('chai').assert;
-var expect = require('chai').expect
-var otx = require('../build');
+var assert = require('chai').assert
+var should = require('chai').should()
+var otx = require('../build')
+
+var API_KEY = process.env.API_KEY
+
+
 
 describe('getAll()', function() {
 
     context('when API Key not present', function() {
         it('should throw an error', function() {
-            expect(otx.getAll.bind({
+            otx.getAll.bind(this).should.throw('No apikey has been passed.')
+
+            otx.getAll.bind(this, {
                 modified_since: '2016-03-18T16:07:29',
                 limit: 10
-            })).to.throw('No apikey has been passed.');
+            }).should.throw('No apikey has been passed.')
+        });
+    });
+
+    context('when API Key is present', function() {
+
+        it('should not throw an error', function(done) {
+            otx.getAll.bind(this, {
+                apikey: 'API_KEY',
+                modified_since: '2016-03-18T16:07:29',
+                limit: 10
+            }).should.not.throw(done)
+        });
+
+        it('should retrive data', function(done) {
+            var stream = otx.getAll({
+                apikey: 'API_KEY',
+                modified_since: '2016-03-18T16:07:29',
+                limit: 1
+            })
+
+            stream.on('data', function(data) {
+                done()
+            })
         });
     });
 
