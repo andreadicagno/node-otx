44ab80f46f2c9818f0dac35890812327c7a3f1ab
Andrea di Cagno <andreadicagno@gmail.com>
2016-07-27 11:37:40 +0200

    modified:   .gitignore  	modified:   package.json  	new file:   test/test.js

---
+++ test/test.js
@@ -0,0 +1,19 @@
+var assert = require('chai').assert;
+var expect = require('chai').expect
+var otx = require('../build');
+
+describe('getAll()', function() {
+
+    context('when API Key not present', function() {
+        it('should throw an error', function() {
+            expect(otx.getAll.bind({
+                modified_since: '2016-03-18T16:07:29',
+                limit: 10
+            })).to.throw('No apikey has been passed.');
+        });
+    });
+
+
+
+
+});
